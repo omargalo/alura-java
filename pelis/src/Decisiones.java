3b225e6ab7dfb71f9b837ef701a186f90e6a457b
Omar García López <omargarcialopez81@outlook.com>
2024-04-18 17:10:06 -0600

    Decisiones

---
+++ pelis/src/Decisiones.java
@@ -0,0 +1,14 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento > 2022) {
+            System.out.println("Películas más populares");
+        }else {
+            System.out.println("Película Retro que aún vale la pena ver");
+        }
+    }
+}
