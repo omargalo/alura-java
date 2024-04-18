edf6badc32d94f305240a062b7ecf8aa08d7fd9f
Omar García López <omargarcialopez81@outlook.com>
2024-04-18 17:18:13 -0600

    Decisiones 2

--- pelis/src/Decisiones.java
+++ pelis/src/Decisiones.java
@@ -2,13 +2,20 @@ public class Decisiones {
 
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElPlan = true;
+        boolean incluidoEnElPlan = false;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
 
-        if (fechaDeLanzamiento > 2022) {
+        if (fechaDeLanzamiento >= 2022) {
             System.out.println("Películas más populares");
         }else {
             System.out.println("Película Retro que aún vale la pena ver");
+        }
+
+        if (incluidoEnElPlan && tipoPlan.equals("plus")) {
+            System.out.println("Disfruta tu película pirata mañosin!");
+        }else {
+            System.out.println("Película no disponible, paga más perro!");
         }
     }
 }
