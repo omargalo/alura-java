d8fd34d279c2f1e782ae819136f7eafedefa1635
Omar García López <omargarcialopez81@outlook.com>
2024-04-18 16:26:14 -0600

    asignando valores

---
+++ pelis/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Bienvenido a Screen Match!");
+        System.out.println("Pelicula: The Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+        double media = (8.2 + 6.0 + 9.0) / 3;
+
+        System.out.println(media);
+
+    }
+}
