55eda99dea76ae15245320c9f4c688ba8f45d92d
Omar García López <omargarcialopez81@outlook.com>
2024-04-19 15:09:39 -0600

    calculos

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,13 +1,21 @@
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
 import com.aluracursos.screenmatch.modelos.Pelicula;
 import com.aluracursos.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
+
         Pelicula miPelicula1 = new Pelicula();
         miPelicula1.setNombre("The Matrix");
         miPelicula1.setAnioDeLanzamiento(1999);
         miPelicula1.setDuracionEnMinutos(120);
         miPelicula1.setIncluidaEnPlan(true);
+
+        Pelicula miPelicula2 = new Pelicula();
+        miPelicula2.setNombre("Interestellar");
+        miPelicula2.setAnioDeLanzamiento(2017);
+        miPelicula2.setDuracionEnMinutos(140);
+        miPelicula2.setIncluidaEnPlan(true);
 
         miPelicula1.muestraFichaTecnica();
         miPelicula1.evalua(9.2);
@@ -26,6 +34,13 @@ public class Principal {
         betterCallSaul.muestraFichaTecnica();
 
         System.out.println(betterCallSaul.getDuracionEnMinutos());
+
+        CalculadoraDeTiempo calculadoraDeTiempo = new CalculadoraDeTiempo();
+        calculadoraDeTiempo.incluye(miPelicula1);
+        calculadoraDeTiempo.incluye(miPelicula2);
+        calculadoraDeTiempo.incluye(betterCallSaul);
+        System.out.println("Tiempo necesario:" + calculadoraDeTiempo.getTiempoTotal());
+
 
     }
 }
