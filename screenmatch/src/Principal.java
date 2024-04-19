0c4a3999264177f99187208c33a6d9369fac81e4
Omar García López <omargarcialopez81@outlook.com>
2024-04-19 16:08:59 -0600

    Arraylist

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -4,6 +4,8 @@ import com.aluracursos.screenmatch.modelos.Episodio;
 import com.aluracursos.screenmatch.modelos.Pelicula;
 import com.aluracursos.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
 
@@ -53,6 +55,24 @@ public class Principal {
         episodio.setTotalVisualizaciones(40);
 
         filtroRecomendacion.filtra(episodio);
+
+        // ArrayList
+
+        var miPelicula3 = new Pelicula();
+        miPelicula3.setNombre("Lord of the Rings");
+        miPelicula3.setAnioDeLanzamiento(2001);
+        miPelicula3.setDuracionEnMinutos(180);
+
+        ArrayList<Pelicula> listaPeliculas = new ArrayList<>();
+        listaPeliculas.add(miPelicula1);
+        listaPeliculas.add(miPelicula2);
+        listaPeliculas.add(miPelicula3);
+
+        System.out.println("Tamaño de la lista: " + listaPeliculas.size());
+        System.out.println("La primera pelicula es: " + listaPeliculas.get(0).getNombre());
+
+        System.out.println(listaPeliculas);
+        System.out.println("toString de la pelicula: " + listaPeliculas.get(0).toString());
 
     }
 }
