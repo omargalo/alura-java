5a846f94037d6b0b7634a700eb2ea3c737552d0e
Omar García López <omargarcialopez81@outlook.com>
2024-04-21 11:41:07 -0600

    Proyecto Tarjeta de Credito

---
+++ ProyectoTarjeta/src/Compra.java
@@ -0,0 +1,31 @@
+// Atributos
+public class Compra implements Comparable<Compra> {
+    private double valor;
+    private String descripcion;
+
+    // Constructor
+    public Compra(double valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    // Getters
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: valor =" + valor +
+                ", descripcion='" + descripcion;
+    }
+
+    @Override
+    public int compareTo(Compra otraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
+    }
+}
