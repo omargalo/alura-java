392b35dc38610c1add9b3e56bb9299517d3a97bc
Omar García López <omargarcialopez81@outlook.com>
2024-04-21 12:36:04 -0600

    Api omdb

---
+++ ApiOmdb/src/Principal.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escribe el nombre de la pelicula: ");
+        var busqueda = lectura.nextLine();
+
+        String direccion = "http://www.omdbapi.com/?t=" + busqueda + "&apikey=efd2e961";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
+}
